fa86c9c733f2b5bc88e02d4d37960b1d9a6cba65
Burra <gezerbugra@gmail.com>
2023-02-14 01:13:36 +0300

    jedi_language_server added

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "clangd", "jdtls"}
+local servers = { "html", "cssls", "tsserver", "clangd", "jdtls", "jedi_language_server"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
