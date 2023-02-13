d7161683ae03ec0e6adda0cfe78e71f7b68a3f7d
Burra <gezerbugra@gmail.com>
2023-02-13 09:52:19 +0300

    css lspserver name fixed

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "css", "tsserver", "clangd", "jdtls"}
+local servers = { "html", "cssls", "tsserver", "clangd", "jdtls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
