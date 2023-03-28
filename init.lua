db23dec590039d2a7656452c21a3729e6345cfc8
Burra <gezerbugra@gmail.com>
2023-03-29 02:08:44 +0300

    hot reload for spring

--- init.lua
+++ init.lua
@@ -4,6 +4,11 @@ end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = set_nvimtree_startup_directory })
 vim.wo.relativenumber = true
 vim.g.nvim_tree_group_empty = 1
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.java" },
+  command = "silent !mvn compile &",
+})
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
